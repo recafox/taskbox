cb724cb4aa48d9f1272df60b6bc0f9c998c01207
ritaaaaa <ritaaaaa@simpleinfo.cc>
2022-03-28 14:04:51 +0800

    GitHub action setup

--- src/lib/store.js
+++ src/lib/store.js
@@ -11,18 +11,18 @@ import {
  * The initial state of our store when the app loads.
  * Usually, you would fetch this from a server. Let's not worry about that now
  */
-const defaultTasks = [
-  { id: "1", title: "Something", state: "TASK_INBOX" },
-  { id: "2", title: "Something more", state: "TASK_INBOX" },
-  { id: "3", title: "Something else", state: "TASK_INBOX" },
-  { id: "4", title: "Something again", state: "TASK_INBOX" },
-];
+
 const TaskBoxData = {
-  tasks: defaultTasks,
+  tasks: [],
   status: "idle",
   error: null,
 };
 
+/*
+ * Creates an asyncThunk to fetch tasks from a remote endpoint.
+ * You can read more about Redux Toolkit's thunks in the docs:
+ * https://redux-toolkit.js.org/api/createAsyncThunk
+ */
 export const fetchTasks = createAsyncThunk("todos/fetchTodos", async () => {
   const response = await fetch(
     "https://jsonplaceholder.typicode.com/todos?userId=1"
@@ -52,28 +52,29 @@ const TasksSlice = createSlice({
         state.tasks[task].state = newTaskState;
       }
     },
-    extraReducers(builder) {
-      builder
-        .addCase(fetchTasks.pending, (state) => {
-          console.log("PENDING");
-          state.status = "loading";
-          state.error = null;
-          state.tasks = [];
-        })
-        .addCase(fetchTasks.fulfilled, (state, action) => {
-          console.log("SUCCEED");
-          state.status = "succeeded";
-          state.error = null;
-          // Add any fetched tasks to the array
-          state.tasks = action.payload;
-        })
-        .addCase(fetchTasks.rejected, (state) => {
-          console.log("REJECTED");
-          state.status = "failed";
-          state.error = "Something went wrong";
-          state.tasks = [];
-        });
-    },
+  },
+  /*
+   * Extends the reducer for the async actions
+   * You can read more about it at https://redux-toolkit.js.org/api/createAsyncThunk
+   */
+  extraReducers(builder) {
+    builder
+      .addCase(fetchTasks.pending, (state) => {
+        state.status = "loading";
+        state.error = null;
+        state.tasks = [];
+      })
+      .addCase(fetchTasks.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.error = null;
+        // Add any fetched tasks to the array
+        state.tasks = action.payload;
+      })
+      .addCase(fetchTasks.rejected, (state) => {
+        state.status = "failed";
+        state.error = "Something went wrong";
+        state.tasks = [];
+      });
   },
 });
 
